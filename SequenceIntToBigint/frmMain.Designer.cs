26d554278c4fd4f2bad2124e0c1f1ae06cd1ad53
madukapai <maduka.pai@gmail.com>
2020-07-21 09:31:32 +0800

    加入專案檔案。

---
+++ SequenceIntToBigint/frmMain.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace SequenceIntToBigint
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDbConn = new System.Windows.Forms.TextBox();
+            this.btnConnDatabase = new System.Windows.Forms.Button();
+            this.lblDbConn = new System.Windows.Forms.Label();
+            this.gvSequence = new System.Windows.Forms.DataGridView();
+            this.lblSeq = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.Convert = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Sequence = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TableName = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.ColumnName = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.gvSequence)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtDbConn
+            // 
+            this.txtDbConn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDbConn.Font = new System.Drawing.Font("細明體", 9F);
+            this.txtDbConn.Location = new System.Drawing.Point(12, 24);
+            this.txtDbConn.Name = "txtDbConn";
+            this.txtDbConn.Size = new System.Drawing.Size(766, 22);
+            this.txtDbConn.TabIndex = 11;
+            // 
+            // btnConnDatabase
+            // 
+            this.btnConnDatabase.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnConnDatabase.Font = new System.Drawing.Font("細明體", 9F);
+            this.btnConnDatabase.Location = new System.Drawing.Point(784, 24);
+            this.btnConnDatabase.Name = "btnConnDatabase";
+            this.btnConnDatabase.Size = new System.Drawing.Size(83, 23);
+            this.btnConnDatabase.TabIndex = 10;
+            this.btnConnDatabase.Text = "Connect";
+            this.btnConnDatabase.UseVisualStyleBackColor = true;
+            this.btnConnDatabase.Click += new System.EventHandler(this.btnConnDatabase_Click);
+            // 
+            // lblDbConn
+            // 
+            this.lblDbConn.AutoSize = true;
+            this.lblDbConn.Font = new System.Drawing.Font("細明體", 9F);
+            this.lblDbConn.Location = new System.Drawing.Point(12, 9);
+            this.lblDbConn.Name = "lblDbConn";
+            this.lblDbConn.Size = new System.Drawing.Size(53, 12);
+            this.lblDbConn.TabIndex = 9;
+            this.lblDbConn.Text = "Database";
+            // 
+            // gvSequence
+            // 
+            this.gvSequence.AllowUserToAddRows = false;
+            this.gvSequence.AllowUserToDeleteRows = false;
+            this.gvSequence.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gvSequence.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvSequence.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Convert,
+            this.Sequence,
+            this.TableName,
+            this.ColumnName});
+            this.gvSequence.Location = new System.Drawing.Point(12, 64);
+            this.gvSequence.Name = "gvSequence";
+            this.gvSequence.RowHeadersVisible = false;
+            this.gvSequence.RowTemplate.Height = 24;
+            this.gvSequence.Size = new System.Drawing.Size(855, 396);
+            this.gvSequence.TabIndex = 12;
+            this.gvSequence.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.gvSequence_CellValueChanged);
+            // 
+            // lblSeq
+            // 
+            this.lblSeq.AutoSize = true;
+            this.lblSeq.Font = new System.Drawing.Font("細明體", 9F);
+            this.lblSeq.Location = new System.Drawing.Point(12, 49);
+            this.lblSeq.Name = "lblSeq";
+            this.lblSeq.Size = new System.Drawing.Size(53, 12);
+            this.lblSeq.TabIndex = 13;
+            this.lblSeq.Text = "Sequence";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnConvert.Location = new System.Drawing.Point(792, 466);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 14;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // Convert
+            // 
+            this.Convert.HeaderText = "Convert";
+            this.Convert.Name = "Convert";
+            this.Convert.Width = 50;
+            // 
+            // Sequence
+            // 
+            this.Sequence.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Sequence.DataPropertyName = "Sequence";
+            this.Sequence.HeaderText = "Sequence";
+            this.Sequence.Name = "Sequence";
+            this.Sequence.ReadOnly = true;
+            // 
+            // TableName
+            // 
+            this.TableName.DataPropertyName = "TableName";
+            this.TableName.HeaderText = "TableName";
+            this.TableName.Name = "TableName";
+            this.TableName.Width = 200;
+            // 
+            // ColumnName
+            // 
+            this.ColumnName.DataPropertyName = "ColumnName";
+            this.ColumnName.HeaderText = "ColumnName";
+            this.ColumnName.Name = "ColumnName";
+            this.ColumnName.Width = 200;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 501);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.lblSeq);
+            this.Controls.Add(this.gvSequence);
+            this.Controls.Add(this.txtDbConn);
+            this.Controls.Add(this.btnConnDatabase);
+            this.Controls.Add(this.lblDbConn);
+            this.Name = "frmMain";
+            this.Text = "SequenceIntToBigint";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gvSequence)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDbConn;
+        private System.Windows.Forms.Button btnConnDatabase;
+        private System.Windows.Forms.Label lblDbConn;
+        private System.Windows.Forms.DataGridView gvSequence;
+        private System.Windows.Forms.Label lblSeq;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Convert;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sequence;
+        private System.Windows.Forms.DataGridViewComboBoxColumn TableName;
+        private System.Windows.Forms.DataGridViewComboBoxColumn ColumnName;
+    }
+}
+
---
+++ SequenceIntToBigint/frmMain.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace SequenceIntToBigint
+{
+    public partial class frmMain : Form
+    {
+        SqlConnection SqlConn = null;
+        SqlDataAdapter SqlAdp = null;
+        List<KeyColumnModel> objColumn, objTable, objKeyColumn;
+        int intConvertIndex, intSequenceIndex, intTableIndex, intColumnIndex;
+        bool blEnableEdit = false;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            txtDbConn.Text = ConfigurationManager.ConnectionStrings["DbConn"].ToString();
+        }
+
+        /// <summary>
+        /// 連接資料庫的動作
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnConnDatabase_Click(object sender, EventArgs e)
+        {
+            blEnableEdit = false;
+
+            // 先連接到資料庫
+            SqlConn = new SqlConnection(txtDbConn.Text);
+
+            // 找出Sequence
+            string strSql = "Select name As Sequence FROM sys.sequences Where maximum_value <= 2147483647 Order By name";
+            SqlAdp = new SqlDataAdapter(strSql, SqlConn);
+            DataTable objSeq = new DataTable();
+            SqlAdp.Fill(objSeq);
+
+            // 找出所有的Table
+            strSql = "Select TABLE_NAME FROM [INFORMATION_SCHEMA].[TABLES]";
+            SqlAdp = new SqlDataAdapter(strSql, SqlConn);
+            DataTable dtTable = new DataTable();
+            SqlAdp.Fill(dtTable);
+            objTable = new List<KeyColumnModel>();
+            for (int i = 0; i < dtTable.Rows.Count; i++)
+            {
+                objTable.Add(
+                    new KeyColumnModel()
+                    {
+                        TableName = dtTable.Rows[i]["TABLE_NAME"].ToString(),
+                    }
+                    );
+            }
+
+            // 找出所有的欄位
+            strSql = "Select * from [INFORMATION_SCHEMA].[COLUMNS]";
+            SqlAdp = new SqlDataAdapter(strSql, SqlConn);
+            DataTable dtColumn = new DataTable();
+            SqlAdp.Fill(dtColumn);
+            objColumn = new List<KeyColumnModel>();
+            for (int i = 0; i < dtColumn.Rows.Count; i++)
+            {
+                objColumn.Add(
+                    new KeyColumnModel()
+                    {
+                        TableName = dtColumn.Rows[i]["TABLE_NAME"].ToString(),
+                        ColumnName = dtColumn.Rows[i]["COLUMN_NAME"].ToString()
+                    }
+                    );
+            }
+
+            // 找出有可能對應的資料表以及欄位
+            strSql = "Select * from [INFORMATION_SCHEMA].[KEY_COLUMN_USAGE]";
+            SqlAdp = new SqlDataAdapter(strSql, SqlConn);
+            DataTable dtKeyColumn = new DataTable();
+            SqlAdp.Fill(dtKeyColumn);
+            objKeyColumn = new List<KeyColumnModel>();
+            for (int i = 0; i < dtKeyColumn.Rows.Count; i++)
+            {
+                objKeyColumn.Add(
+                    new KeyColumnModel()
+                    {
+                        TableName = dtKeyColumn.Rows[i]["TABLE_NAME"].ToString(),
+                        ColumnName = dtKeyColumn.Rows[i]["COLUMN_NAME"].ToString()
+                    }
+                    );
+            }
+
+            // 放入到GridView之中
+            gvSequence.DataSource = objSeq;
+            for (int i = 0; i < gvSequence.Columns.Count; i++)
+            {
+                if (gvSequence.Columns[i].Name == "Sequence")
+                    intSequenceIndex = i;
+
+                if (gvSequence.Columns[i].Name == "TableName")
+                    intTableIndex = i;
+
+                if (gvSequence.Columns[i].Name == "ColumnName")
+                    intColumnIndex = i;
+
+                if (gvSequence.Columns[i].Name == "Convert")
+                    intConvertIndex = i;
+            }
+
+
+            // 依序放入有可能對應的資料表以及其欄位
+            for (int i = 0; i < gvSequence.Rows.Count; i++)
+            {
+                string strSequence = gvSequence.Rows[i].Cells[intSequenceIndex].Value.ToString();
+                DataGridViewComboBoxCell ddlTable = (DataGridViewComboBoxCell)gvSequence.Rows[i].Cells[intTableIndex];
+                DataGridViewComboBoxCell ddlColumn = (DataGridViewComboBoxCell)gvSequence.Rows[i].Cells[intColumnIndex];
+                DataGridViewCheckBoxCell cbxConvert = (DataGridViewCheckBoxCell)gvSequence.Rows[i].Cells[intConvertIndex];
+
+                string strTableName = objKeyColumn.Where(x => x.ColumnName == strSequence).Select(c => c.TableName).FirstOrDefault() ?? "";
+
+                ddlTable.DataSource = objTable.Select(c => c.TableName).ToList();
+                ddlTable.Value = strTableName;
+
+                // 放入Column
+                if (!string.IsNullOrEmpty(strTableName))
+                {
+                    ddlColumn.DataSource = objColumn.Where(x => x.TableName == strTableName).Select(c => c.ColumnName).ToList();
+                    ddlColumn.Value = objKeyColumn.Where(x => x.TableName == strTableName).Select(c => c.ColumnName).FirstOrDefault() ?? "";
+
+                    // 打勾
+                    cbxConvert.Value = true;
+                }
+            }
+
+            blEnableEdit = true;
+        }
+
+        /// <summary>
+        /// 欄位的值有變更的動作
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void gvSequence_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == intTableIndex && blEnableEdit)
+            {
+                string strTableName = gvSequence.Rows[e.RowIndex].Cells[intTableIndex].Value.ToString();
+
+                // 放入Column
+                if (!string.IsNullOrEmpty(strTableName))
+                {
+                    DataGridViewComboBoxCell ddlColumn = (DataGridViewComboBoxCell)gvSequence.Rows[e.RowIndex].Cells[intColumnIndex];
+                    ddlColumn.DataSource = objColumn.Where(x => x.TableName == strTableName).Select(c => c.ColumnName).ToList();
+                    ddlColumn.Value = objKeyColumn.Where(x => x.TableName == strTableName).Select(c => c.ColumnName).FirstOrDefault() ?? "";
+                }
+            }
+        }
+
+        /// <summary>
+        /// 進行類型轉換
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            // 先找出當下的順序代碼
+            string strSql = "Select name As Sequence, current_value FROM sys.sequences Where maximum_value <= 2147483647 Order By name";
+            SqlAdp = new SqlDataAdapter(strSql, SqlConn);
+            DataTable objSeq = new DataTable();
+            SqlAdp.Fill(objSeq);
+            List<SequenceValueModel> objSeqValue = new List< SequenceValueModel>();
+            for (int i=0; i<objSeq.Rows.Count; i++)
+            {
+                objSeqValue.Add(new SequenceValueModel()
+                {
+                    CurrentValue = int.Parse(objSeq.Rows[i]["current_value"].ToString()),
+                    Sequence = objSeq.Rows[i]["Sequence"].ToString(),
+                });
+            }
+
+            SqlCommand SqlCmd = new SqlCommand();
+            SqlCmd.Connection = SqlConn;
+            // SqlCmd.Connection.Open();
+
+            for (int i = 0; i < gvSequence.Rows.Count; i++)
+            {
+                string strSequence = gvSequence.Rows[i].Cells[intSequenceIndex].Value.ToString();
+                DataGridViewComboBoxCell ddlTable = (DataGridViewComboBoxCell)gvSequence.Rows[i].Cells[intTableIndex];
+                DataGridViewComboBoxCell ddlColumn = (DataGridViewComboBoxCell)gvSequence.Rows[i].Cells[intColumnIndex];
+                DataGridViewCheckBoxCell cbxConvert = (DataGridViewCheckBoxCell)gvSequence.Rows[i].Cells[intConvertIndex];
+
+                // 進行更新動作
+                if ((bool)cbxConvert.Value)
+                {
+                    // 先刪除Sequence
+                    strSql = "DROP SEQUENCE " + strSequence;
+                    SqlCmd.CommandText = strSql;
+                    // SqlCmd.ExecuteNonQuery();
+
+                    // 重新建立Sequence
+                    int intCurrentValue = objSeqValue.Where(x => x.Sequence == strSequence).Select(c => c.CurrentValue).FirstOrDefault();
+                    strSql = "CREATE SEQUENCE " + strSequence + " As bigint CYCLE INCREMENT BY 1 START WITH " + intCurrentValue.ToString();
+                    SqlCmd.CommandText = strSql;
+                    // SqlCmd.ExecuteNonQuery();
+
+                    // 更新資料表
+                    //ALTER TABLE[dbo].[USER] DROP CONSTRAINT[PK_USER_USER_ID]
+
+                    //ALTER TABLE[dbo].[USER] ALTER COLUMN USER_ID bigint NOT NULL
+
+
+                    //ALTER TABLE[dbo].[USER] ADD CONSTRAINT[PK_USER_USER_ID] PRIMARY KEY CLUSTERED
+                    //(
+                    //[USER_ID] ASC
+                    //)WITH(PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON[PRIMARY]
+                }
+            }
+        }
+
+        public class KeyColumnModel
+        {
+            public string TableName { get; set; }
+            public string ColumnName { get; set; }
+        }
+
+        public class SequenceValueModel
+        {
+            public string Sequence { get; set; }
+            public int CurrentValue { get; set; }
+        }
+    }
+}
